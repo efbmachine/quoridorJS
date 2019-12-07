70bb37aed2eadd8ac97534d4961e544de55f0bd7
chuck <efbmachine06@gmail.com>
2019-12-07 06:20:11 +0100

    added server folder

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const Express = require('express')();
+const Http = require('http').Server(Express);
+const Socketio = require('socket.io')(Http);
+
+let port  = 3001;
+
+Socketio.on('connection',(socket)=>{
+    console.log('A new user just connected ' +socket.id)
+    socket.on('placeWall',(position)=>{
+        if(position.match(/\w\d\w/)){
+            console.log(socket.id +':'+position)
+            socket.emit('placeWall',position)
+        }else{
+            socket.emit('info',{msg:"wrong move"})
+            console.log('wrong position')
+        }
+
+    })
+    socket.on("disconnect", () => console.log("Client disconnected"));
+})
+
+
+Http.listen(port, ()=>{
+    console.log(`Server running on port:${port}`)
+})
