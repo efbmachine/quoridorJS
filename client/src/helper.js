3cecf5109233b7b133cee48312b8d3166eaa3640
chuck <efbmachine06@gmail.com>
2019-12-23 17:48:30 +0100

    changed the position of the comments

--- client/src/helper.js
+++ client/src/helper.js
@@ -6,12 +6,6 @@ export const posToArr = (position)=>{
     let row = positions[1].charCodeAt(0) - 96
     return ({'row':row,'col':Number(positions[2])-1,'orr':positions[3].toLowerCase()})
 }
-// export const arrToPos = (arr)=>{
-//     let regex = /(\d)(\d)(\w?)/
-//     let arrs = arr.match(regex)
-//     let row = String.fromCharCode(97 + arrs[1])
-//     return (row+''+(Number(arrs[2])+1)+arrs[3])
-// }
 
 export const arrToPos =(arr)=>{
     console.log(arr)
@@ -22,3 +16,10 @@ export const arrToPos =(arr)=>{
     return result
 
 }
+
+// export const arrToPos = (arr)=>{
+//     let regex = /(\d)(\d)(\w?)/
+//     let arrs = arr.match(regex)
+//     let row = String.fromCharCode(97 + arrs[1])
+//     return (row+''+(Number(arrs[2])+1)+arrs[3])
+// }
