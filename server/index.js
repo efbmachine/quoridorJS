021f99ffcb57a255852c7bcf5827635872252691
chuck <efbmachine06@gmail.com>
2019-12-23 18:06:34 +0100

    added functions for walls and positioning

--- server/index.js
+++ server/index.js
@@ -156,3 +156,57 @@ class Player{
         this.justPlayed =true
     }
 }
+
+const blockedWays = (wall) =>{
+    let pos = posToArr(wall)
+    if(pos.orr=='h'){
+        // example:  f3h: f3-f2; g3-g2
+        // means the offset is -1 for col and then +1 for row
+        let path = {};
+        //  TO SEE THE KEYS OF path USE Object.entries(path)
+        let p1 = pos.row+''+pos.col,
+            p2 = pos.row +''+(pos.col-1);
+        let p3 = (pos.row+1) + '' +pos.col,
+            p4 = (pos.row+1) + '' +(pos.col-1);
+        path[p1]= p2
+        path[p3]= p4
+        return Object.entries(path)
+    }
+    else{
+        // example: f3v: f3-g3:f4-g4
+        //means the offset is +1 for row and then +1 for col
+        let path = {};
+        let p1 = pos.row     + '' + pos.col,//f3
+            p2 = (pos.row+1) + '' + pos.col;//g3
+        let p3 = (pos.row)   + '' + pos.col+1,//f4
+            p4 = (pos.row+1) + '' + (pos.col+1);//g4
+        path[p1]= p2;
+        path[p3]= p4;
+        return Object.entries(path)
+    }
+}
+
+const isPlaceable = (wall, walls) =>{
+    walls.forEatch((item)=>{
+        if(item.position == wall.position)
+            return false;
+        return true;
+    })
+}
+
+export const posToArr = (position)=>{
+    let regex = /(\w)(\d)(\w?)/
+    let positions = position.match(regex)
+    let row = positions[1].charCodeAt(0) - 96
+    return ({'row':row,'col':Number(positions[2])-1,'orr':positions[3].toLowerCase()})
+}
+
+export const arrToPos =(arr)=>{
+    console.log(arr)
+    let row = String.fromCharCode(96 + Number(arr[0]))
+    let col = Number(arr[1])
+    let result = row+''+col
+    console.log(result)
+    return result
+
+}
